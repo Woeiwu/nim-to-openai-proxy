7910f7f709f952659f281af9f032858041ddcf7f
Woeiwu <kylekristoffersonbase@gmail.com>
2026-09-21 06:59:32 +0800

    Update server.js

--- server.js
+++ server.js
@@ -86,7 +86,8 @@ const MODEL_MAPPING = {
   'google-light': 'google/gemma-4-31b-it',
   'google-lightest': 'meta/muse-glimmer-30b', // was google/gemma-2b
   'google-lighter': 'poolside/laguna-xs-2.1', // was google/gemma-3-4b-it
-  'm3': 'minimaxai/minimax-m3'
+  'm3': 'minimaxai/minimax-m3',
+  '8x22b': 'mistralai/mixtral-8x22b-instruct'
 };
 
 // Used when an unrecognized alias is requested. Must point at a live model.
